20ef9f97120f1e4870f814c4ba278dcc3f74a2c9
BarkerEmily <emilybarker@byui.edu>
2023-11-07 09:41:38 -0700

    step 6

--- controllers/users.js
+++ controllers/users.js
@@ -11,9 +11,16 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('users').find();
+    const result = await mongodb.getDatabase().db().collection('users').find({
+        _id: userId
+    });
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users[0]);
     });
+};
+
+module.exports = {
+    getAll,
+    getSingle
 };
