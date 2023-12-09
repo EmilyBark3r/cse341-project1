6d9020d14b411ac3fe65bd001f476fd1fd203523
BarkerEmily <emilybarker@byui.edu>
2023-12-09 10:32:34 -0700

    CRUD project and Oath

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
+const passport = require('passport');
 
-router.use('/', require('./swagger'));
+router.use("/", require("./swagger"));
+// router.use("/courses", require("./courses"));
+// router.use("/instructors", require("./instructors"));
 
-router.get('/', (req, res) => {
-    //#swagger.tags={'Hello World'}
-    res.send('Hello World')
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+router.get('/logout', function (req, res, next) {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
 });
-
-router.use('/users', require('./users'));
-
 module.exports = router;
---
+++ routes/instructors.js
@@ -0,0 +1,16 @@
+// const router = require('express').Router();
+
+// const getInstructorsController = require("../controllers/instructors");
+
+// const {
+//     isAuthenticated
+// } = require("../middleware/authenticate");
+
+// router.get("/", getInstructorsController.getAllInstructors);
+// router.get("/:id", getInstructorsController.getInstructors);
+// router.post("/", isAuthenticated, getInstructorsController.postInstructors);
+// router.put("/:id", isAuthenticated, getInstructorsController.putInstructors);
+// router.delete("/:id", isAuthenticated, getInstructorsController.deleteInstructors);
+
+
+// module.exports = router;
